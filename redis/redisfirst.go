8d4852d2579f43d660b7a156858db6f319cc9f8e
EXT02D16367 <mehmet.tatar@consultant.turkcell.com.tr>
2020-04-13 14:03:43 +0300

    reducer shared memory some redis

---
+++ redis/redisfirst.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "redis", // no password set
+		DB:       0,       // use default DB
+	})
+	// err := client.Set("key", "value", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// val, err := client.Get("key").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("key", val)
+
+	// val2, err := client.Get("key2").Result()
+	// if err == redis.Nil {
+	// 	fmt.Println("key2 does not exist")
+	// } else if err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Println("key2", val2)
+	// }
+
+	// i, _ := client.RPush("mylist", "deneme123").Result()
+
+	// client.RPush("mylist2", "{client:deneme}")
+	// t, _ := client.LRange("mylist2", 0, 1).Result()
+	// fmt.Println(t)
+	// client.RPop("mylist2").Result() // 1 elemt çıkarıyor sağdan
+
+	// client.LPush("mylist", "soldan eklendi").Result()
+	// client.LTrim("mylist", 0, 2).Result() // son eklenen 3 item dışındakileri siler
+
+	l, _ := client.BRPop(time.Second*50, "mylist3").Result() // block the code if list null and wait when some item added mylist3
+	fmt.Println(l)
+
+}
