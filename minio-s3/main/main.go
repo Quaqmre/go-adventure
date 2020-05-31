dd300169a0ecd7b7343f19eb52dc9a164b95056f
EXT02D16367 <mehmet.tatar@consultant.turkcell.com.tr>
2020-05-31 16:48:56 +0300

    minio example

---
+++ minio-s3/main/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"github.com/minio/minio-go"
+)
+
+func main() {
+	endpoint := "localhost:9000"
+	accessKeyID := "testkey"
+	secretAccessKey := "secretkey"
+	useSSL := false
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Make a new bucket called mymusic.
+	bucketName := "mymusic"
+	location := "us-east-1"
+
+	err = minioClient.MakeBucket(bucketName, location)
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := minioClient.BucketExists(bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+
+	// Upload the zip file
+	objectName := "temp.file"
+	filePath := "./temp.file"
+	contentType := "application/file"
+
+	// Upload the zip file with FPutObject
+	n, err := minioClient.FPutObject(bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
+}
--- strings/isany.go
+++ strings/isany.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	s := "qwes"
-	bigs := "aaaaaaaaaaaaaaaaaas"
+	bigs := "aaaaaaaaaaaaaaaaaas" //input lengt 8 den büyükse 128 karakter için 8 bytlık 8 lenghtlik bir array aoluşturuğ 128 bit
+	//set edilerek olan harfleri bu bitlere set ediliyor
 	b := strings.IndexAny(bigs, s)
 	fmt.Println(b)
 }
